a926d37cc2fc150b8ca1aff8730fce5a90239197
victor.govorov <victorgovorov@mail.ru>
2020-12-17 23:01:31 +0500

    added DbDiagnosticObserver

---
+++ src/DbDiagnosticObserver.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Data.Diagnostics.Payloads;
+
+namespace Microsoft.Data.Diagnostics
+{
+    public abstract class DbDiagnosticObserver :
+        IObserver<DiagnosticListener>,
+        IObserver<KeyValuePair<string, object?>>,
+        IDisposable
+    {
+        private IDisposable? _executing;
+        private readonly List<IDisposable> _subscriptions = new List<IDisposable>();
+        
+        private readonly Dictionary<string, Action<DbDiagnosticObserver, object>> _methods = new Dictionary<string, Action<DbDiagnosticObserver, object>>
+        {
+            {DbDiagnosticListener.EventNames.CommandExecuting, (observer, value) => observer.OnCommandExecuting((CommandPayload)value)},
+            {DbDiagnosticListener.EventNames.CommandExecuted, (observer, value) => observer.OnCommandExecuted((CommandPayload)value)},
+            {DbDiagnosticListener.EventNames.CommandExecutingError, (observer, value) => observer.OnCommandError((CommandPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionOpening, (observer, value) => observer.OnConnectionOpening((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionOpened, (observer, value) => observer.OnConnectionOpened((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionOpeningError, (observer, value) => observer.OnConnectionOpeningError((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionClosing, (observer, value) => observer.OnConnectionClosing((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionClosed, (observer, value) => observer.OnConnectionClosed((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.ConnectionClosingError, (observer, value) => observer.OnConnectionClosingError((ConnectionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionCommitting, (observer, value) => observer.OnTransactionCommitting((TransactionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionCommitted, (observer, value) => observer.OnTransactionCommitted((TransactionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionCommittingError, (observer, value) => observer.OnTransactionCommittingError((TransactionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionRollingBack, (observer, value) => observer.OnTransactionRollingBack((TransactionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionRolledBack, (observer, value) => observer.OnTransactionRolledBack((TransactionPayload)value)},
+            {DbDiagnosticListener.EventNames.TransactionRollingBackError, (observer, value) => observer.OnTransactionRollingBackError((TransactionPayload)value)}
+        };
+
+        public void Start()
+        {
+            if (_executing != null)
+                throw new InvalidOperationException("DbDiagnosticObserver is already started");
+
+            _executing = DiagnosticListener.AllListeners.Subscribe(this);
+        }
+
+        public void Stop()
+        {
+            if (_executing != null)
+            {
+                _executing.Dispose();
+                _executing = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void OnStart()
+        {
+        }
+
+        protected virtual void OnStop()
+        {
+        }
+
+        protected virtual bool OnNext(string eventName, object? payload)
+        {
+            return string.IsNullOrWhiteSpace(eventName) == false && payload != null;
+        }
+
+        protected virtual void OnCommandExecuting(CommandPayload payload)
+        {
+        }
+
+        protected virtual void OnCommandExecuted(CommandPayload payload)
+        {
+        }
+
+        protected virtual void OnCommandError(CommandPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionOpening(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionOpened(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionOpeningError(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionClosing(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionClosed(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnConnectionClosingError(ConnectionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionCommitting(TransactionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionCommitted(TransactionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionCommittingError(TransactionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionRollingBack(TransactionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionRolledBack(TransactionPayload payload)
+        {
+        }
+
+        protected virtual void OnTransactionRollingBackError(TransactionPayload payload)
+        {
+        }
+
+        void IObserver<DiagnosticListener>.OnNext(DiagnosticListener diagnosticListener)
+        {
+            if (diagnosticListener.Name == nameof(DbDiagnosticListener))
+                _subscriptions.Add(diagnosticListener.Subscribe(this));
+        }
+
+        void IObserver<DiagnosticListener>.OnCompleted()
+        {
+            _subscriptions.ForEach(x => x.Dispose());
+            _subscriptions.Clear();
+        }
+
+        void IObserver<DiagnosticListener>.OnError(Exception error)
+        {
+        }
+
+        void IObserver<KeyValuePair<string, object?>>.OnCompleted()
+        {
+        }
+
+        void IObserver<KeyValuePair<string, object?>>.OnError(Exception error)
+        {
+        }
+
+        void IObserver<KeyValuePair<string, object?>>.OnNext(KeyValuePair<string, object?> value)
+        {
+            if (OnNext(value.Key, value.Value))
+                return;
+
+            if (_methods.TryGetValue(value.Key, out var method) == false)
+                return;
+
+            method(this, value.Value!);
+        }
+    }
+}
